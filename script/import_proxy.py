f13177916322bd417c3779b3721db89dc69519ff
linjixiong <linjixiong@fabepa.com>
2019-12-10 12:46:45 +0800

    add import proxy

---
+++ script/import_proxy.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3.6
+
+import sys, getopt
+import csv
+import json
+import xml
+import urllib.parse
+import urllib.request
+import datetime
+import random
+
+
+def post( hostport, url, data ):
+    urlpath = f"{hostport}{url}"
+    headers = { 'accept' : 'application/json', 'Content-Type': 'application/json' }
+    body = bytes(json.dumps(data), 'utf8')
+    req = urllib.request.Request(urlpath, body, headers)
+    response = urllib.request.urlopen(req)
+    result = response.read()
+    d=result.decode("utf8")
+    j = json.loads(d)
+    return j
+
+def import_proxy(hostport,recordfile,tag):
+    data = {}
+    with open(namefile, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=':')
+        records = []
+        first = True
+        for row in reader:
+            host = row[0]
+            record = {}
+            record['host'] = host
+            record['port'] = port
+            records.append(record)
+        data["proxies"] = records
+        data["tag"] = tag        
+        rsp = post(hostport, "/api/v1/proxies", data)
+        print(rsp)
+   
+def main(argv):
+   help = "import_proxy.py -i <ifile> -u <url> -t <tag>"
+   try:
+      opts, args = getopt.getopt(argv,"hi:u:t:",["ifile=","url=","tag="])
+   except getopt.GetoptError:
+      print(help)
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print(help)
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-t", "--tag"):
+         tag = arg
+      elif opt in ("-u", "--url"):
+         url = arg
+   import_proxy(url, inputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
